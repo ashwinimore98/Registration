79e43857db24f8dc1abef3e785ac5dd8ec234328
ashwini more <more.ashg98@gmail.com>
2022-08-25 15:00:30 +0530

    msg

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace UserRegistration
+{
+    class Program
+    {
+        public static string REGEX_PATTERN = "^[A-Z]{1}[a-zA-Z]{2,}";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome_To_UserRegistration_RegeEx");
+            FirstNameVal();
+            LastNameVal();
+        }
+        public static void FirstNameVal()
+        {
+            Regex rg = new Regex(REGEX_PATTERN);
+            Console.Write("Enter First Name: ");
+            string first_name = Console.ReadLine();
+            bool validate = rg.IsMatch(first_name);
+            if (validate)
+                Console.WriteLine("Name is Valid : " + first_name);
+            else
+                Console.WriteLine("Invalid FirstName");
+        }
+        public static void LastNameVal()
+        {
+            Regex rg = new Regex(REGEX_PATTERN);
+            Console.Write("Enter Last Name: ");
+            string last_name = Console.ReadLine();
+            bool validate = rg.IsMatch(last_name);
+            if (validate)
+                Console.WriteLine("Name is Valid : " + last_name);
+            else
+                Console.WriteLine("Invalid LastName");
+        }
+    }
+}
